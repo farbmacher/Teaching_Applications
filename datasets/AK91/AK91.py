64b2be54e79ecbcd28f813c99180f139d315a956
Michael Mühlegger <michael.muehlegger@tum.de>
2021-12-08 14:44:12 +0100

    ak91

--- datasets/AK91/AK91.py
+++ datasets/AK91/AK91.py
@@ -1,23 +1,6 @@
 import pandas as pd
 import numpy as np
 
-def get_dummy_names(ak91):
-    """
-    returns a tupel containing 6 lists of variable names
-    corresponding to all dummy variables for 
-    (QOB, YOB, POB, IQP, IQY, IQYP), facilitating access 
-    to certain groups of dummy variables e.g. all quarter of birth dummies
-    """
-    
-    QOB = list(ak91.columns)[66:69]
-    YOB = list(ak91.columns)[57:66]
-    POB = list(ak91.columns)[6:56]
-
-    IQP = list(ak91.columns)[70:220]
-    IQY = list(ak91.columns)[220:247]
-    IQYP= list(ak91.columns)[247:]
-    return (QOB, YOB, POB, IQP, IQY, IQYP)
-
 def prepare_data():
     """
     Reads the original dataset from AK91 (QOB_tableV.dta) and creates
@@ -56,3 +39,20 @@ def prepare_data():
     #print("QOB, YOB, POB, QOB+QOB*YOB+QOB*POB, QOB+QOB*YOB+QOB*POB+QOB*YOB*POB")
     len(QOB), len(YOB), len(POB), len(QOB)+len(QOB)*len(YOB)+len(QOB)*len(POB), len(IQYP)+len(IQP)+len(IQY)+len(QOB)
     return AK91
+
+def get_dummy_names(ak91):
+    """
+    returns a tupel containing 6 lists of variable names
+    corresponding to all dummy variables for 
+    (QOB, YOB, POB, IQP, IQY, IQYP), facilitating access 
+    to certain groups of dummy variables e.g. all quarter of birth dummies
+    """
+    
+    QOB = list(ak91.columns)[66:69]
+    YOB = list(ak91.columns)[57:66]
+    POB = list(ak91.columns)[6:56]
+
+    IQP = list(ak91.columns)[70:220]
+    IQY = list(ak91.columns)[220:247]
+    IQYP= list(ak91.columns)[247:]
+    return (QOB, YOB, POB, IQP, IQY, IQYP)
